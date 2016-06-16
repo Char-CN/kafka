aa16c1d9ee1656e507c7e1ed0830acecb40f0182
hyy <hyy@192.168.112.1>
2016-06-16 16:27:10 +0800

    commit

--- src/main/java/org/blazer/kafka/KafkaConsumerTest.java
+++ src/main/java/org/blazer/kafka/KafkaConsumerTest.java
@@ -18,36 +18,26 @@ public class KafkaConsumerTest {
 
 	private BlockingQueue<String> queue = new ArrayBlockingQueue<String>(10);
 	private final ConsumerConnector consumer;
-	private final String TOPIC = "my-topic";
+	private final String TOPIC = "hyy-test";
 
 	private KafkaConsumerTest() {
 		Properties props = new Properties();
-		// zookeeper 配置
 		props.put("zookeeper.connect", "ms:2181,sl1:2181,sl2:2181");
-
-		// group 代表一个消费组
 		props.put("group.id", "hyy-group");
-
-		// zk连接超时
 		props.put("zookeeper.session.timeout.ms", "4000");
 		props.put("zookeeper.sync.time.ms", "200");
 		props.put("auto.commit.interval.ms", "1000");
 		props.put("auto.offset.reset", "smallest");
-		// 序列化类
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
-
 		ConsumerConfig config = new ConsumerConfig(props);
-
 		consumer = kafka.consumer.Consumer.createJavaConsumerConnector(config);
 	}
 
 	void consume() {
 		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
 		topicCountMap.put(TOPIC, new Integer(1));
-
 		StringDecoder keyDecoder = new StringDecoder(new VerifiableProperties());
 		StringDecoder valueDecoder = new StringDecoder(new VerifiableProperties());
-
 		Map<String, List<KafkaStream<String, String>>> consumerMap = consumer.createMessageStreams(topicCountMap, keyDecoder, valueDecoder);
 		KafkaStream<String, String> stream = consumerMap.get(TOPIC).get(0);
 		ConsumerIterator<String, String> it = stream.iterator();
--- src/main/java/org/blazer/kafka/KafkaProducerTest.java
+++ src/main/java/org/blazer/kafka/KafkaProducerTest.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class KafkaProducerTest {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "ms:9092,sl1:9092,sl2:9092");
@@ -19,11 +20,16 @@ public class KafkaProducerTest {
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
 		Producer<String, String> producer = new KafkaProducer<String, String>(props);
-		ProducerRecord re = new ProducerRecord<String, String>("my-topic", "key000000000000000000000000000000", "value111111111111111111111111111");
+		ProducerRecord re = new ProducerRecord<String, String>("hyy-test", "k2", "hyy good");
+		System.out.println(re);
 		producer.send(re);
 		for (int i = 0; i < 5; i++) {
-			ProducerRecord record = new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i));
-			System.out.println(record.topic() + "	" + i);
+			int k = 0;
+			if (i % 2 == 0) {
+				k = 1;
+			}
+			ProducerRecord record = new ProducerRecord<String, String>("hyy-test", "k" + k, "hello word");
+			System.out.println(record);
 			producer.send(record);
 		}
 		producer.close();
